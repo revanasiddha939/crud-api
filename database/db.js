15b3e2d44dc63d0d79ad84bc47cab7bc18bdc721
revanasiddha939 <66510046+revanasiddha939@users.noreply.github.com>
2023-07-30 12:57:44 +0530

    Updated db.js

--- database/db.js
+++ database/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-	db: 'mongodb+srv://malagerevanasiddha:revanu123@cluster0.hg9quwd.mongodb.net/cluster0?retryWrites=true&w=majority'
+	db: 'mongodb+srv://malagerevanasiddha:SOQPSwsAn4gPPCG7@cluster0.hg9quwd.mongodb.net/cluster0?retryWrites=true&w=majority'
 };
